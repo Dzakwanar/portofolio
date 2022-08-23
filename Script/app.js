b8e336f8e14b82c0246cbad96ddd858f6d857d40
Dzakwanar <104410175+Dzakwanar@users.noreply.github.com>
2022-08-23 22:49:41 +0700

    project
    project untuk dicoding

---
+++ Script/app.js
@@ -0,0 +1,7 @@
+alert("selamat datang di programnya dzakwan")
+var respond = prompt("kalau boleh tau nama anda siapa? : ");
+if(respond != "")
+alert("Hallo " + respond + " ! " + "Selamat datang " + respond); 
+else 
+alert("Pengguna Tidak Memasukkan Kalimat Apapun");
+
